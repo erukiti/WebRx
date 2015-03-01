6e5b821d49c00fcf85476ff93a574427fdad53bf
Oliver Weichhold <oliver@weichhold.com>
2015-03-01 20:43:55 +0100

    Nested module test

--- test/Directives/ModuleDirective.ts
+++ test/Directives/ModuleDirective.ts
@@ -8,28 +8,41 @@ describe('Directives', () => {
             loadFixtures('templates/Directives/Module.html');
 
             // register a directive that would not be available in default module
-            var m = wx.module("test");
-            m.registerDirective("foo", wx.app.getDirective("text"));
+            wx.module("test").registerDirective("foo", wx.app.getDirective("text"));
 
             var el = document.querySelector("#module-name");
             var model = {};
             expect(() => wx.applyDirectives(model, el)).not.toThrow();
 
-            expect($(el).find("#content").text()).toEqual("bar");
+            expect($(el).children("#content").text()).toEqual("bar");
         });
 
         it('binds using module instance',() => {
             loadFixtures('templates/Directives/Module.html');
 
             // register a directive that would not be available in default module
-            var m = wx.module("test");
-            m.registerDirective("foo", wx.app.getDirective("text"));
+            wx.module("test").registerDirective("foo", wx.app.getDirective("text"));
 
             var el = document.querySelector("#module-instance");
             var model = { module: wx.module("test") };
             expect(() => wx.applyDirectives(model, el)).not.toThrow();
 
-            expect($(el).find("#content").text()).toEqual("bar");
+            expect($(el).children("#content").text()).toEqual("bar");
         });
-});
+
+        it('nested modules should work',() => {
+            loadFixtures('templates/Directives/Module.html');
+
+            // register a directive that would not be available in default module
+            wx.module("test1").registerDirective("foo", wx.app.getDirective("text"));
+            wx.module("test2").registerDirective("bar", wx.app.getDirective("text"));
+
+            var el = document.querySelector("#module-name-nested");
+            var model = { };
+            expect(() => wx.applyDirectives(model, el)).not.toThrow();
+
+            expect($(el).children("#content").text()).toEqual("bar");
+            expect($("#child-container").children("#content").text()).toEqual("bar");
+        });
+    });
 });
