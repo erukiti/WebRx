9ce111a180a1c4d93d1ce3c58aca406fd6c7998e
Oliver Weichhold <oliver@weichhold.com>
2015-03-03 19:28:04 +0100

    Improved forEach directive

--- src/Directives/ForEachDirective.ts
+++ src/Directives/ForEachDirective.ts
@@ -5,6 +5,53 @@
 /// <reference path="../Core/Resources.ts" />
 
 module wx {
+    export interface IForEachDirectiveOptions {
+        data: any;
+        hooks?: IForEachDirectiveHooks|string;
+    }
+
+    export interface IForEachDirectiveHooks {
+        /** 
+        * Is invoked each time the foreach block is duplicated and inserted into the document, 
+        * both when foreach first initializes, and when new entries are added to the associated 
+        * array later
+        **/
+        afterRender?(nodes: Node[], data: any): void;
+
+        /** 
+        * Is like afterRender, except it is invoked only when new entries are added to your array 
+        * (and not when foreach first iterates over your array’s initial contents).
+        * A common use for afterAdd is to call a method such as jQuery’s $(domNode).fadeIn() 
+        * so that you get animated transitions whenever items are added
+        **/
+        afterAdd? (nodes: Node[], data: any, index: number): void;
+
+        /** 
+        * Is invoked when an array item has been removed, but before the corresponding 
+        * DOM nodes have been removed. If you specify a beforeRemove callback, then it 
+        * becomes your responsibility to remove the DOM nodes. The obvious use case here 
+        * is calling something like jQuery’s $(domNode).fadeOut() to animate the removal 
+        * of the corresponding DOM nodes — in this case, WebRx cannot know how soon 
+        * it is allowed to physically remove the DOM nodes (who knows how long your 
+        * animation will take?)
+        **/
+        beforeRemove? (nodes: Node[], data: any, index: number): void;
+
+        /** 
+        * Is invoked when an array item has changed position in the array, but before 
+        * the corresponding DOM nodes have been moved. You could use beforeMove 
+        * to store the original screen coordinates of the affected elements so that you 
+        * can animate their movements in the afterMove callback.
+        **/
+        beforeMove? (nodes: Node[], data: any, index: number): void;
+
+        /** 
+        * Is invoked after an array item has changed position in the array, and after 
+        * foreach has updated the DOM to match.
+        **/
+        afterMove? (nodes: Node[], data: any, index: number): void;
+    }
+
     class ForEachDirective implements IDirective {
         constructor(domService: IDomService) {
             this.domService = domService;
@@ -23,9 +70,27 @@ module wx {
             var el = <HTMLElement> node;
             var self = this;
             var initialApply = true;
-            var exp = <ICompiledExpression> options;
-            var obs = this.domService.expressionToObservable(exp, ctx);
             var cleanup: Rx.CompositeDisposable = null;
+            var hooks: IForEachDirectiveHooks|string;
+            var exp: ICompiledExpression;
+
+            if (typeof options === "object" && options.hasOwnProperty("data")) {
+                var opt = <IForEachDirectiveOptions> options;
+
+                exp = opt.data;
+
+                using(this.domService.expressionToObservable(<ICompiledExpression> opt.hooks, ctx).toProperty(),(prop) => {
+                    hooks = prop();
+                });
+
+                // optionally resolve hooks if passed as string identifier
+                if (typeof hooks === "string")
+                    hooks = injector.resolve<IForEachDirectiveHooks>(<string> hooks);
+            } else {
+                exp = options;
+            }
+
+            var obs = this.domService.expressionToObservable(exp, ctx);
 
             // add own disposables
             state.cleanup.add(Rx.Disposable.create(() => {
@@ -46,7 +111,7 @@ module wx {
 
                 cleanup = new Rx.CompositeDisposable();
 
-                self.applyValue(el, x, template, ctx, initialApply, cleanup);
+                self.applyValue(el, x, hooks, template, ctx, initialApply, cleanup);
                 initialApply = false;
             }));
 
@@ -65,6 +130,7 @@ module wx {
 
                 // nullify locals
                 template = null;
+                hooks = null;
             }));
         }
 
@@ -92,7 +158,7 @@ module wx {
             }).startWith(startIndex);
         }
 
-        protected  clear(el: HTMLElement, indexes?: IWeakMap<Node, Rx.Observable<any>>) {
+        protected clear(el: HTMLElement, hooks: IForEachDirectiveHooks, indexes?: IWeakMap<Node, Rx.Observable<any>>) {
             var node: Node;
 
             while (el.firstChild) {
@@ -110,24 +176,31 @@ module wx {
         }
 
         protected appendAll(el: HTMLElement, list: IObservableList<any>, ctx: IDataContext,
-            template: Array<Node>, indexes: IWeakMap<Node, Rx.Observable<any>>, recalcIndextrigger: Rx.Observable<any>) {
+            template: Array<Node>, hooks: IForEachDirectiveHooks, isInitial: boolean,
+            indexes: IWeakMap<Node, Rx.Observable<any>>, recalcIndextrigger: Rx.Subject<any>) {
             var length = list.length;
 
             for (var i = 0; i < length; i++) {
-                this.appendRow(el, i, list.get(i), ctx, template, recalcIndextrigger, indexes);
+                this.appendRow(el, i, list.get(i), ctx, template, hooks, isInitial, recalcIndextrigger, indexes);
             }
         }
 
         protected appendRow(el: HTMLElement, index: number, item: any, ctx: IDataContext, template: Array<Node>,
-            recalcIndextrigger?: Rx.Observable<any>, indexes?: IWeakMap<Node, Rx.Observable<any>>): void {
+            hooks: IForEachDirectiveHooks, isInitial?: boolean, recalcIndextrigger?: Rx.Subject<any>,
+            indexes?: IWeakMap<Node, Rx.Observable<any>>): void {
             var _index: any = undefined;
             var created = false;
             var templateLength = template.length;
-            var node: Node;            
+            var node: Node;
+            var added = [];            
 
             for (var i = 0; i < templateLength; i++) {
                 node = template[i].cloneNode(true);
+
                 el.appendChild(node);
+
+                if (hooks)
+                    added.push(node);
 
                 if (node.nodeType === 1) {
                     if (recalcIndextrigger && !created) {
@@ -150,19 +223,32 @@ module wx {
                     this.domService.applyDirectives(item, node);
                 }
             }
+
+            if (hooks) {
+                if (hooks.afterRender)
+                    hooks.afterRender(added, item);
+
+                if (!isInitial && hooks.afterAdd)
+                    hooks.afterAdd(added, item, index);
+            }
         }
 
         protected insertRow(el: HTMLElement, index: number, item: any, ctx: IDataContext, template: Array<Node>,
-            recalcIndextrigger?: Rx.Observable<any>, indexes?: IWeakMap<Node, Rx.Observable<any>>): void {
+            hooks: IForEachDirectiveHooks, recalcIndextrigger?: Rx.Subject<any>, indexes?: IWeakMap<Node, Rx.Observable<any>>): void {
             var _index: any = undefined;
             var created = false;
             var templateLength = template.length;
             var node: Node;
             var refNode = <Node> el.children[templateLength * index];
+            var added = [];
 
             for (var i = 0; i < templateLength; i++) {
                 node = template[i].cloneNode(true);
+
                 el.insertBefore(node, refNode);
+
+                if (hooks)
+                    added.push(node);
 
                 if (node.nodeType === 1) {
                     if (recalcIndextrigger && !created) {
@@ -185,21 +271,103 @@ module wx {
                     this.domService.applyDirectives(item, node);
                 }
             }
+
+            if (hooks) {
+                if(hooks.afterRender)
+                    hooks.afterRender(added, item);
+
+                if (hooks.afterAdd)
+                    hooks.afterAdd(added, item, index);
+            }
         }
 
-        protected removeRow(el: HTMLElement, index: number, template: Array<Node>, indexes: IWeakMap<Node, Rx.Observable<any>>): void {
+        protected removeRow(el: HTMLElement, index: number, item: any, template: Array<Node>, hooks: IForEachDirectiveHooks,
+            recalcIndextrigger: Rx.Subject<any>, indexes: IWeakMap<Node, Rx.Observable<any>>): void {
             var templateLength = template.length;
             var nodexIndex = index * templateLength;
+            var toBeRemoved = [];
 
             for (var i = 0; i < templateLength; i++) {
-                var node = el.childNodes[nodexIndex];
+                var node = el.childNodes[nodexIndex + i];
 
                 if (node.nodeType === 1) {
                     this.domService.cleanNode(node);
                     indexes.delete(node);
                 }
 
-                el.removeChild(node);
+                toBeRemoved.push(node);
+            }
+
+            if (hooks && hooks.beforeRemove) {
+                hooks.beforeRemove(toBeRemoved, item, index);
+            } else {
+                for (i = 0; i < toBeRemoved.length; i++) {
+                    el.removeChild(toBeRemoved[i]);
+                }
+            }
+        }
+
+        protected moveRow(el: HTMLElement, from: number, to: number, item: any, template: Array<Node>, hooks: IForEachDirectiveHooks,
+            recalcIndextrigger: Rx.Subject<any>, indexes: IWeakMap<Node, Rx.Observable<any>>): void {
+            var templateLength = template.length;
+            var nodexIndex = from * templateLength;
+            var nodes = [];
+
+            for (var i = 0; i < templateLength; i++) {
+                var node = el.childNodes[nodexIndex + i];
+
+                if (node.nodeType === 1) {
+                    this.domService.cleanNode(node);
+                    indexes.delete(node);
+                }
+
+                nodes.push(node);
+            }
+
+            if (hooks && hooks.beforeMove) {
+                hooks.beforeMove(nodes, item, from);
+            }
+
+            for (i = 0; i < nodes.length; i++) {
+                el.removeChild(nodes[i]);
+            }
+
+            var refNode = <Node> el.children[templateLength * to];
+            var _index: any = undefined;
+            var created = false;
+
+            for (i = 0; i < templateLength; i++) {
+                node = template[i].cloneNode(true);
+
+                el.insertBefore(node, refNode);
+
+                if (hooks)
+                    nodes.push(node);
+
+                if (node.nodeType === 1) {
+                    if (recalcIndextrigger && !created) {
+                        // create index observable
+                        _index = indexes.get(node);
+
+                        if (!_index) {
+                            _index = this.createIndexObservableForNode(el, node, to, recalcIndextrigger, templateLength);
+                            indexes.set(node, _index);
+                            created = true;
+                        }
+                    }
+
+                    // propagate index to state
+                    var state = this.domService.createNodeState(item);
+                    state.properties.index = _index || to;
+                    this.domService.setNodeState(node, state);
+
+                    // done
+                    this.domService.applyDirectives(item, node);
+                }
+            }
+
+            if (hooks && hooks.afterMove) {
+                hooks.afterMove(nodes, item, from);
             }
         }
 
@@ -227,7 +395,8 @@ module wx {
             }
         }
         
-        protected monitorList(el: HTMLElement, ctx: IDataContext, template: Array<Node>, cleanup: Rx.CompositeDisposable, list: IObservableList<any>) {
+        protected monitorList(el: HTMLElement, ctx: IDataContext, template: Array<Node>, cleanup: Rx.CompositeDisposable,
+            list: IObservableList<any>, hooks: IForEachDirectiveHooks) {
             var i: number;
             var length: number;
 
@@ -238,7 +407,7 @@ module wx {
             cleanup.add(indexTrigger);
 
             // initial insert
-            this.appendAll(el, list, ctx, template, indexes, indexTrigger);
+            this.appendAll(el, list, ctx, template, hooks, true, indexes, indexTrigger);
 
             // track changes
             cleanup.add(list.itemsAdded.subscribe((e) => {
@@ -246,11 +415,11 @@ module wx {
 
                 if (e.from === list.length) {
                     for (i = 0; i < length; i++) {
-                        this.appendRow(el, i + e.from, e.items[i], ctx, template, indexTrigger, indexes);
+                        this.appendRow(el, i + e.from, e.items[i], ctx, template, hooks, false, indexTrigger, indexes);
                     }
                 } else {
                     for (i = 0; i < e.items.length; i++) {
-                        this.insertRow(el, i + e.from, e.items[i], ctx, template, indexTrigger, indexes);
+                        this.insertRow(el, i + e.from, e.items[i], ctx, template, hooks, indexTrigger, indexes);
                     }
                 }
 
@@ -261,16 +430,15 @@ module wx {
                 length = e.items.length;
 
                 for (i = 0; i < length; i++) {
-                    this.removeRow(el, i + e.from, template, indexes);
+                    this.removeRow(el, i + e.from, e.items[i], template, hooks, indexTrigger, indexes);
                 }
 
                 indexTrigger.onNext(true);
             }));
 
             cleanup.add(list.itemsMoved.subscribe((e) => {
-                this.removeRow(el, e.from, template, indexes);
-                this.insertRow(el, e.to, e.items[0], ctx, template, indexTrigger, indexes);
-
+                this.moveRow(el, e.from, e.to, e.items[0], template, hooks, indexTrigger, indexes);
+ 
                 indexTrigger.onNext(true);
             }));
 
@@ -281,14 +449,14 @@ module wx {
             }));
 
             cleanup.add(list.shouldReset.subscribe((e) => {
-                this.clear(el, indexes);
-                this.appendAll(el, list, ctx, template, indexes, indexTrigger);
+                this.clear(el, hooks, indexes);
+                this.appendAll(el, list, ctx, template, hooks, false, indexes, indexTrigger);
 
                 indexTrigger.onNext(true);
             }));
         }
 
-        protected applyValue(el: HTMLElement, value: any, template: Array<Node>,
+        protected applyValue(el: HTMLElement, value: any, hooks: IForEachDirectiveHooks, template: Array<Node>,
             ctx: IDataContext, initialApply: boolean, cleanup: Rx.CompositeDisposable): void {
             var i, length;
 
@@ -301,7 +469,7 @@ module wx {
                 }
             }
 
-            this.clear(el);
+            this.clear(el, hooks);
 
             if (Array.isArray(value)) {
                 var arr = <Array<any>> value;
@@ -310,12 +478,12 @@ module wx {
                 length = arr.length;
 
                 for (i = 0; i < length; i++) {
-                    this.appendRow(el, i, arr[i], ctx, template);
+                    this.appendRow(el, i, arr[i], ctx, template, hooks);
                 }
             } else if(utils.isList(value)) {
                 var list = <IObservableList<any>> value;
 
-                this.monitorList(el, ctx, template, cleanup, list);
+                this.monitorList(el, ctx, template, cleanup, list, hooks);
             } else {
                 internal.throwError("forEach binding only operates on IObservableList or standard Javascript-Arrays");
             }
--- test/Directives/ForEachDirective.ts
+++ test/Directives/ForEachDirective.ts
@@ -106,18 +106,18 @@ describe('Directives',() => {
             list.set(2, new TestViewModel(42, "magic"));
             expect(parseInt(el.children[2 * templateLength].textContent)).toEqual(list.get(2).foo());
 
-            // check indexes
+            // verify indexes
             expect($(el).children(".part3").map((index, node) => parseInt(node.textContent)).get()).toEqual(Ix.Enumerable.range(0, list.length).toArray());
 
             list.move(2, 0);
             expect(parseInt(el.children[0 * templateLength].textContent)).toEqual(list.get(0).foo());
 
-            // check indexes
+            // verify indexes
             expect($(el).children(".part3").map((index, node) => parseInt(node.textContent)).get()).toEqual(Ix.Enumerable.range(0, list.length).toArray());
 
             list.removeAt(1);
 
-            // check indexes
+            // verify indexes
             expect($(el).children(".part3").map((index, node) => parseInt(node.textContent)).get()).toEqual(Ix.Enumerable.range(0, list.length).toArray());
 
             expect(parseInt(el.children[0 * templateLength].textContent)).toEqual(list.get(0).foo());
@@ -127,7 +127,7 @@ describe('Directives',() => {
             list.move(0, 2);
             expect(parseInt(el.children[2 * templateLength].textContent)).toEqual(list.get(2).foo());
 
-            // check indexes
+            // verify indexes
             expect($(el).children(".part3").map((index, node) => parseInt(node.textContent)).get()).toEqual(Ix.Enumerable.range(0, list.length).toArray());
 
             list.clear();
@@ -137,13 +137,13 @@ describe('Directives',() => {
             expect(parseInt(el.children[0 * templateLength].textContent)).toEqual(list.get(0).foo());
             expect(el.children[0 * templateLength + 1].textContent).toEqual(list.get(0).bar());
 
-            // check indexes
+            // verify indexes
             expect($(el).children(".part3").map((index, node) => parseInt(node.textContent)).get()).toEqual(Ix.Enumerable.range(0, list.length).toArray());
 
             list.addRange(createTestList().toArray());
             expect(el.children.length).toEqual(list.length * templateLength);
 
-            // check indexes
+            // verify indexes
             expect($(el).children(".part3").map((index, node) => parseInt(node.textContent)).get()).toEqual(Ix.Enumerable.range(0, list.length).toArray());
         });
 
@@ -155,15 +155,16 @@ describe('Directives',() => {
             var list = createTestList();
             expect(() => wx.applyDirectives({ src: list }, el)).not.toThrowError();
 
-            // check indexes
+            // verify indexes
             expect($(el).children(".part3").map((index, node) => parseInt(node.textContent)).get()).toEqual(Ix.Enumerable.range(0, list.length).toArray());
 
             list.add(new TestViewModel(3, "edfsd"));
-            // check indexes
+            // verify indexes
             expect($(el).children(".part3").map((index, node) => parseInt(node.textContent)).get()).toEqual(Ix.Enumerable.range(0, list.length).toArray());
 
             list.removeAt(0);
-            // check indexes
+
+            // verify indexes
             expect($(el).children(".part3").map((index, node) => parseInt(node.textContent)).get()).toEqual(Ix.Enumerable.range(0, list.length).toArray());
         });
 
@@ -177,6 +178,139 @@ describe('Directives',() => {
 
             list.forEach((x) => x.foo(33));
             expect($(el).children(".part1").map((index, node) => parseInt(node.textContent)).get()).toEqual(list.map(x=> x.foo()));
+        });
+
+        it('observable list with hooks',() => {
+            loadFixtures('templates/Directives/ForEach.html');
+
+            var el = <HTMLElement> document.querySelector("#foreach-list-model-with-index-and-hooks");
+            var templateLength = el.children.length;
+            var list = createTestList();
+            var beforeRemoveCount = 0;
+            var afterRenderCount = 0;
+            var afterAddCount = 0;
+            var beforeMoveCount = 0;
+            var afterMoveCount = 0;
+
+            var hooks: wx.IForEachDirectiveHooks = {
+                afterRender: (nodes: Node[], data: any) => {
+                    afterRenderCount++;
+                },
+                afterAdd: (nodes: Node[], data: any, index: number) => {
+                    afterAddCount++;
+                },
+                beforeRemove: (nodes: Node[], data: any, index: number) => {
+                    beforeRemoveCount++;
+
+                    nodes.forEach(node => node.parentNode.removeChild(node));
+                },
+                beforeMove: (nodes: Node[], data: any, index: number) => {
+                    beforeMoveCount++;
+                },
+                afterMove: (nodes: Node[], data: any, index: number) => {
+                    afterMoveCount++;
+                }
+            };
+
+            expect(() => wx.applyDirectives({ src: list, hooks: hooks }, el)).not.toThrowError();
+
+            expect(afterRenderCount).toEqual(3);
+
+            list.add(new TestViewModel(3, "edfsd"));
+            expect(afterAddCount).toEqual(1);
+
+            // verify indexes
+            expect($(el).children(".part3").map((index, node) => parseInt(node.textContent)).get()).toEqual(Ix.Enumerable.range(0, list.length).toArray());
+
+            list.removeAt(0);
+            expect(beforeRemoveCount).toEqual(1);
+
+            // verify indexes
+            expect($(el).children(".part3").map((index, node) => parseInt(node.textContent)).get()).toEqual(Ix.Enumerable.range(0, list.length).toArray());
+
+            list.move(2, 0);
+            expect(beforeMoveCount).toEqual(1);
+            expect(afterMoveCount).toEqual(1);
+
+            // verify indexes
+            expect($(el).children(".part3").map((index, node) => parseInt(node.textContent)).get()).toEqual(Ix.Enumerable.range(0, list.length).toArray());
+        });
+
+        it('observable list manipulation smoke-test with async-hooks',() => {
+            loadFixtures('templates/Directives/ForEach.html');
+
+            var el = <HTMLElement> document.querySelector("#foreach-list-model-with-index-and-hooks");
+            var templateLength = el.children.length;
+            var list = createTestList();
+
+            var hooks: wx.IForEachDirectiveHooks = {
+                afterRender: (nodes: Node[], data: any) => {
+                },
+                afterAdd: (nodes: Node[], data: any, index: number) => {
+                },
+                beforeRemove: (nodes: Node[], data: any, index: number) => {
+                    var disp = Rx.Observable.timer(1000).subscribe(x => {
+                        nodes.forEach(node => node.parentNode.removeChild(node));
+
+                        disp.dispose();
+                    });
+                },
+                beforeMove: (nodes: Node[], data: any, index: number) => {
+                },
+                afterMove: (nodes: Node[], data: any, index: number) => {
+                }
+            };
+
+            expect(() => wx.applyDirectives({ src: list, hooks: hooks }, el)).not.toThrowError();
+
+            expect(el.children.length).toEqual(list.length * templateLength);
+            expect(parseInt(el.children[0 * templateLength].textContent)).toEqual(list.get(0).foo());
+
+            list.add(new TestViewModel(3, "edfsd"));
+            expect(el.children.length).toEqual(list.length * templateLength);
+
+            list.set(2, new TestViewModel(42, "magic"));
+            expect(parseInt(el.children[2 * templateLength].textContent)).toEqual(list.get(2).foo());
+
+            // verify indexes
+            expect($(el).children(".part3").map((index, node) => parseInt(node.textContent)).get()).toEqual(Ix.Enumerable.range(0, list.length).toArray());
+
+            list.move(2, 0);
+            expect(parseInt(el.children[0 * templateLength].textContent)).toEqual(list.get(0).foo());
+
+            // verify indexes
+            expect($(el).children(".part3").map((index, node) => parseInt(node.textContent)).get()).toEqual(Ix.Enumerable.range(0, list.length).toArray());
+
+            list.removeAt(1);
+
+            // verify indexes
+            expect($(el).children(".part3").map((index, node) => parseInt(node.textContent)).get()).toEqual(Ix.Enumerable.range(0, list.length).toArray());
+
+            expect(parseInt(el.children[0 * templateLength].textContent)).toEqual(list.get(0).foo());
+            expect(parseInt(el.children[1 * templateLength].textContent)).toEqual(list.get(1).foo());
+            expect(parseInt(el.children[2 * templateLength].textContent)).toEqual(list.get(2).foo());
+
+            list.move(0, 2);
+            expect(parseInt(el.children[2 * templateLength].textContent)).toEqual(list.get(2).foo());
+
+            // verify indexes
+            expect($(el).children(".part3").map((index, node) => parseInt(node.textContent)).get()).toEqual(Ix.Enumerable.range(0, list.length).toArray());
+
+            list.clear();
+            expect(el.children.length).toEqual(0);
+
+            list.add(new TestViewModel(42, "magic"));
+            expect(parseInt(el.children[0 * templateLength].textContent)).toEqual(list.get(0).foo());
+            expect(el.children[0 * templateLength + 1].textContent).toEqual(list.get(0).bar());
+
+            // verify indexes
+            expect($(el).children(".part3").map((index, node) => parseInt(node.textContent)).get()).toEqual(Ix.Enumerable.range(0, list.length).toArray());
+
+            list.addRange(createTestList().toArray());
+            expect(el.children.length).toEqual(list.length * templateLength);
+
+            // verify indexes
+            expect($(el).children(".part3").map((index, node) => parseInt(node.textContent)).get()).toEqual(Ix.Enumerable.range(0, list.length).toArray());
         });
     });
 });
