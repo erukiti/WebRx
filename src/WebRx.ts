216ed9cde0896aa903b4ebcdad6393af10477d81
Oliver Weichhold <oliver@weichhold.com>
2015-06-12 13:04:53 +0200

    Cleanup

--- src/WebRx.ts
+++ src/WebRx.ts
@@ -18,8 +18,7 @@ export { route } from "./Routing/RouteMatcher";
 export { getNodeValue, setNodeValue } from "./Bindings/Value";
 export { injector } from "./Core/Injector";
 export { default as IID } from "./IID";
-export { SingleOneWayBindingBase } from "./Bindings/BindingBase";
-export { MultiOneWayBindingBase } from "./Bindings/BindingBase";
+export { SingleOneWayBindingBase, MultiOneWayBindingBase } from "./Bindings/BindingBase";
 
 // re-exports
 import * as res from "./Core/Resources";
