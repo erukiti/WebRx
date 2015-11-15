44ef5fa9fdb1c3b3c8ab989654e366294c4db379
Oliver Weichhold <oliver@weichhold.com>
2015-11-15 15:48:41 +0100

    Fixed IE version detection

--- src/Core/Environment.ts
+++ src/Core/Environment.ts
@@ -31,7 +31,7 @@ let version = document && (function () {
 
     // Keep constructing conditional HTML blocks until we hit one that resolves to an empty fragment
     while (
-        div.innerHTML = '<!--[if gt wx.IE ' + (++version) + ']><i></i><![endif]-->',
+        div.innerHTML = '<!--[if gt IE ' + (++version) + ']><i></i><![endif]-->',
         iElems[0]
         ) { }
     return version > 4 ? version : undefined;
